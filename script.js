89cd802d0692b9267821214bbf9d94ec41ffd761
Amiran <o2@Mac-mini-O2.local>
2022-01-27 17:46:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+    if(navigator.geolocation)
+    {
+        navigator.geolocation.getCurrentPosition(position => {
+            const keyId = 'a1a5763c6ce3ed3ae0df7930f0d187b2';
+            const lang = 'ru'
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${keyId}&lang=${lang}`;
+            fetch(api)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                console.log(data);
+                document.querySelector('.location__city').textContent = data.name;
+                document.querySelector('.temperature__degree').innerHTML = Math.floor(data.main.temp - 273) + '&deg';
+                console.log(document.querySelector('.location__icon').innerHTML = data.weather[0].icon);
+                document.querySelector('.temperature__descrip').innerHTML = data.weather[0].description;
+            })
+        });
+    }else
+    {
+        h1.textContent = 'hey dis is not working bec ause resons'
+    }
+});
